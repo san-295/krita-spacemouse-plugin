3b086147849d705b1d1056f6b2179a460e759ae8
Songyang An <songyang769@gmail.com>
2025-08-24 12:36:57 +1200

    updated pathing

--- krita_spacemouse/extension.py
+++ krita_spacemouse/extension.py
@@ -1,9 +1,9 @@
-# extension.py - Ultra-minimal SpaceMouse extension
+# extension.py - Main SpaceMouse extension controller
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QMessageBox, QDockWidget
 from PyQt5 import QtCore
 from krita import Extension, Krita, DockWidgetFactory, DockWidgetFactoryBase
-from .spnav import libspnav
+from .models.spnav import libspnav
 from .event_handler import poll_spacenav
 
 class SpacenavControlExtension(Extension):
@@ -16,7 +16,7 @@ class SpacenavControlExtension(Extension):
     def setup(self):
         # Create factory function that captures extension reference
         def create_spacenav_docker():
-            from .docker import SpacenavDocker
+            from .views.docker import SpacenavDocker
             return SpacenavDocker(self)
         
         # Create and register factory
@@ -43,8 +43,8 @@ class SpacenavControlExtension(Extension):
         try:
             dockers = Krita.instance().dockers()
             for docker in dockers:
-                if docker.objectName() == "spacenavDocker" and hasattr(docker, 'advanced_tab'):
-                    poll_rate = docker.advanced_tab.get_poll_rate()
+                if docker.objectName() == "spacenavDocker" and hasattr(docker, 'configuration_tab'):
+                    poll_rate = docker.configuration_tab.get_poll_rate()
                     break
         except Exception as e:
             QtCore.qDebug(f"Could not get poll rate from docker, using default: {e}")
